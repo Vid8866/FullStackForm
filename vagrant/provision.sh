927f972a4de59c5cdb9d242fe9ad552ce024d177
Vid Grozdanov <vid.grozdanov@gmail.com>
2025-11-23 22:07:20 +0100

    Vagrantfile and provision.sh fixed

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-APP_DIR="/home/vagrant/FullStackForm/application/app"
-cd "$APP_DIR"
-
 echo "==============================="
 echo " Updating system packages..."
 echo "==============================="
 sudo apt update -y
 
-
 echo "==============================="
 echo " Installing system dependencies..."
 echo "==============================="
-sudo apt install -y python3-pip python3-venv
+sudo apt install -y python3-pip python3-venv redis-server postgresql postgresql-contrib
+
+echo "==============================="
+echo " Setting up application..."
+echo "==============================="
+git clone https://github.com/Vid8866/FullStackForm.git
+cd FullStackForm/application/app
 
 python3 -m venv venv
 source venv/bin/activate
@@ -22,14 +24,11 @@ pip install -r requirements.txt
 echo "==============================="
 echo " Configuring PostgreSQL..."
 echo "==============================="
-
-sudo apt install -y postgresql postgresql-contrib
 sudo -u postgres psql <<EOF
 CREATE USER "user" WITH PASSWORD 'pass';
 CREATE DATABASE demo OWNER "user";
 GRANT ALL PRIVILEGES ON DATABASE demo TO "user";
 EOF
-
 
 echo "==============================="
 echo " Setting up Redis..."
@@ -37,25 +36,9 @@ echo "==============================="
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
 
-
 echo "==============================="
-echo " Configuring Nginx reverse proxy..."
+echo " Running the application..."
 echo "==============================="
-
-sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
-server {
-    listen 80;
-
-    location / {
-        proxy_pass http://127.0.0.1:5001;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-    }
-}
-EOF
-
-sudo systemctl restart nginx
-
 python3 main.py
 
 echo "==============================="
